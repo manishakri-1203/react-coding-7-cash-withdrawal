3c6424708cd0be00eaf864fb655134a79c62bd70
manisha <manisha.kri2000@gmail.com>
2024-03-04 07:12:36 +0000

    not updating value

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import './index.css'
+
+const DenominationItem = props => {
+  const {denominationDetails, updateBalance} = props
+  const {value} = denominationDetails
+
+  const onClickDenomination = () => {
+    updateBalance(value)
+  }
+
+  return (
+    <li className="Denomination-item">
+      <button
+        className="denomination-btn"
+        type="button"
+        onClick={onClickDenomination}
+      >
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationItem
